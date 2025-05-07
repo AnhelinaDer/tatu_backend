3343aa44923d2ac8ad6eb6ea9100f04900bf8e96
AnhelinaDer <angdereng@gmail.com>
2025-05-08 00:43:22 +0200

    Connected to prisma

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const { Prisma } = require('@prisma/client');
+const express = require('express');
+const router = express.Router();
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async(req, res, next) => {
+  const users = prisma.user.findMany();
   res.render('index', { title: 'Express' });
 });
 
